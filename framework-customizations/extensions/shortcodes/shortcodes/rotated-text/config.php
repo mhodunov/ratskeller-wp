28e2222c348e2d91cf735039015f39e6babce577
Maksym Hodunov <m.godunov@yandex.ru>
2021-06-17 14:41:47 +0300

    feat: adds some cusom shortcodes

---
+++ framework-customizations/extensions/shortcodes/shortcodes/rotated-text/config.php
@@ -0,0 +1,13 @@
+<?php if (!defined('FW')) {
+    die('Forbidden');
+}
+
+
+$cfg = array(
+    'page_builder' => array(
+        'title'         => __('Rotated Text', 'Page builder section'),
+        'description'   => __('Small additional header', 'Page builder section'),
+        'tab'           => __('Content Elements', 'Page builder section'),
+        'popup_size'    => 'small',
+    )
+);
